3c8bc8d239a8a1b380d6140a5bd174b4559c4384
ericBlack1 <ericangeloawa@gmail.com>
2025-06-12 04:14:38 +0100

    render db

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:angeloboy@localhost/waste_management"
+    DATABASE_URL: str = "postgresql+asyncpg://waste_management_r6dq_user:pc3cDMMJivdyungesGDSAothImmbCjkU@dpg-d154aep5pdvs73esfsmg-a/waste_management_r6dq"
     SECRET_KEY: str = "your-secret-key-here"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
